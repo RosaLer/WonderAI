948d286986699b3e5d72947176cb0c181fc91815
RosaLer <rosalerma@outlook.es>
2025-04-27 20:31:44 +0200

    Docker composed

--- src/backend/run_chroma.py
+++ src/backend/run_chroma.py
@@ -1,6 +1,6 @@
 from chromadb.app import app
 import uvicorn
-import os 
+import os
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)  
